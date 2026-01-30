d0440f066b4d9fa9bce95bcfdca8ef29eda9f82f
shji47603 <shji47603@gmail.com>
2026-01-30 20:15:39 +0300

    fix

--- src/consts/backendRoutes.ts
+++ src/consts/backendRoutes.ts
@@ -1,4 +1,4 @@
-export const basicRoute = "https://wish-backend-l4oibd2q8-shji162s-projects.vercel.app/?_vercel_share=3DUYaAv7OgTOqTH7Ip8JTgojRIZiDlk6"
+export const basicRoute = "https://wish-backend-l4oibd2q8-shji162s-projects.vercel.app/?_vercel_share=3DUYaAv7OgTOqTH7Ip8JTgojRIZiDlk6/"
 
 export const authRoute = basicRoute + "auth/"
 export const wishRoute = basicRoute + "wishes/"
